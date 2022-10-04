ff2795e8cd50adfba388132de734fbad8c873413
adyyang2002 <adyyang02@gmail.com>
2022-10-04 01:53:53 -0400

    huh

--- market.py
+++ market.py
@@ -1,4 +1,12 @@
 import requests
+# get prices from dydx
+# make a dydx account
+# buy/sell on dydx programatically
+# install dydx client on the aws instance
+# track how much time i spend on it
+# learn git?
+# understand public/private key cryptography
+# what is rsa algorithm wiki
 
 # defining key/request url
 key = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
